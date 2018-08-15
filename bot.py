508d441f08fd5476a35ec25756a28d3c14291a74
yldrmzffr <muzaffer@yldrmzffr.com>
2018-08-16 01:49:51 +0300

    bildirim alma özelliği eklendi

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ FORMAT = '[%(asctime)s] [%(levelname)s] [%(message)s]'
 logging.basicConfig(filename="logs.txt", format=FORMAT, level=logging.INFO)
 logging.info("Server Basladi.")
 
-cevap = False
+bildirim = False
 bot = telebot.TeleBot("<<TELEGRAM-BOT-TOKEN>>")
 data = {}
 
@@ -18,44 +18,63 @@ with open("data", "r") as gelen:
 logging.info("Mesajlar Oluşturuldu")
 
 
+def bildirimal(message):
+    global bildirim
+    with open("bildirimler.txt","a") as bilfile:
+        bilfile.write("[{}] [{}] [{}] \n".format(message.from_user.username,message.from_user.first_name+message.from_user.last_name,message.text))
+    bot.send_message(message.chat.id,"{}({}) Bildirimini aldık belki şuan okuyoruz. En yakın sürede dönüş/düzenleme yapılacak.".format(message.from_user.first_name,message.from_user.username))
+    bildirim = False
+
+
+def cevapver(message):
+    for i in data.keys():
+        if message.text.lower().find(i) > -1:
+            bot.send_message(message.chat.id,data.get(i))
+            logging.info("{} ile ilgili Cevap Gönderildi".format(i))
+            return True
+    return False
+
+
+def ceydayasor(message):
+    try:
+        arg = {'username': '<<CEYD-A-USERNAME>>', 'token': '<<CEYD-A-TOKEN>>','code': message.text.lower() ,'type':'text'}
+        r= requests.post("http://beta.ceyd-a.com/jsonengine.jsp", data=arg).content.decode('utf-8')[1:-3]
+        cevap = json.loads(r).get("answer")
+
+        if cevap != '':
+            bot.send_message(message.chat.id,cevap)
+            logging.info("Ceyd-a cevap verdi : {}".format(cevap))
+        else:
+            bot.send_message(message.chat.id,"Ceyd-A API Beta sürümünde çalıştığı için bazı komutlarda eksilik var. Play Store dan tam sürümü indirip bu komutu tekrar deneyebilirsin.")
+            logging.info("Ceyd-a boş yapti. : {}".format(r))
+
+    except Exception as e:
+        bot.send_message(message.chat.id,"Bir hata oluştu ve bildirildi. En yakın zamanda düzeltilecek.")
+        logging.error(e)
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     logging.info("-{}- mesajı alındı.".format(message.text))
     bot.send_message(message.chat.id,"Merhaba {} nasıl yadımcı olabilirim?".format(message.from_user.first_name))
     logging.info("Hoşgeldin Mesajı Gönderildi.")
 
+@bot.message_handler(commands=['bildirim'])
+def send_welcome(message):
+    global bildirim
+    logging.info("Bildirim alınacak.")
+    bot.send_message(message.chat.id,"Merhaba {}. İstek dilek ve Şikayetlerini Buraya Yazabilirsin".format(message.from_user.first_name))
+    bildirim = True
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    global cevap
+    global bildirim
     logging.info("{}- mesajı alındı.".format(message.text))
-    gelen = message.text.lower()
-
-    for i in data.keys():
-        if gelen.find(i) > -1:
-            bot.send_message(message.chat.id,data.get(i))
-            cevap = True
-            logging.info("{} ile ilgili Cevap Gönderildi".format(i))
-
-    if cevap == False:
-        logging.info("Cevap Bulunamadı Ceyd-a ya soruluyor..")
-        try:
-
-            arg = {'username': '<<CEYD-A-USERNAME>>', 'token': '<<CEYD-A-TOKEN>>','code': gelen ,'type':'text'}
-            r= requests.post("http://beta.ceyd-a.com/jsonengine.jsp", data=arg).content.decode('utf-8')[1:-3]
-            cevap = json.loads(r).get("answer")
-
-            if cevap != '':
-                bot.send_message(message.chat.id,cevap)
-                logging.info("Ceyd-a cevap verdi : {}".format(cevap))
-            else:
-                bot.send_message(message.chat.id,"Ceyd-A API Beta sürümünde çalıştığı için bazı komutlarda eksilik var. Play Store dan tam sürümü indirip bu komutu tekrar deneyebilirsin.")
-                logging.info("Ceyd-a boş yapti. : {}".format(r))
-
-        except Exception as e:
-            bot.send_message(message.chat.id,"Bir hata oluştu ve bildirildi. En yakın zamanda düzeltilecek.")
-            logging.error(e)
-
-    cevap = False
+    if bildirim == True:
+        bildirimal(message)
+    else:
+        if cevapver(message) == False:
+            logging.info("Cevap Bulunamadı Ceyd-a ya soruluyor..")
+            ceydayasor(message)
 
 bot.polling()
